fbfcf25ea8c63d666424ed7a4327a5cff0907125
Denny <Dennyng1989@hotmail.com>
2023-02-28 15:38:59 -0800

    Organize if/else logic

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,17 +3,20 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
   request(url , (error, response, body) => {
-    let desc;
+    let desc = JSON.parse(body)[0];
     
     if (error) {
-      desc = null;
-    } else if (JSON.parse(body)[0] === undefined) {
+      callback(error, null);
+      return;
+    }
+    
+    if (!desc) {
       desc = "Breed not found!";
     } else {
-      desc = JSON.parse(body)[0].description;
+      desc = desc.description;
     }
 
-    callback(error, desc);
+    callback(null, desc);
   });
 };
 
