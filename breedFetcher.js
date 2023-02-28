9b0c6a1cfe37ff6810be813cd49105e08eb8dff4
Denny <Dennyng1989@hotmail.com>
2023-02-28 01:21:28 -0800

    refactor function to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,20 @@
 const request = require('request');
 
-// Keep only search term
-const search = process.argv[2];
-// concatenate base api URL with search term
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + search;
-
-request(url, (error, response, body) => {
-  // log code only if error does not occur
-  if (error) {
-    console.log('Connection error! See error code below:');
-    console.log(response && response.statusCode);
-  } else {
-    // Check if 0th index is undefined
-    if (JSON.parse(body)[0] === undefined) {
-      console.log("Breed not found!");
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url , (error, response, body) => {
+    let desc;
+    
+    if (error) {
+      desc = null;
+    } else if (JSON.parse(body)[0] === undefined) {
+      desc = "Breed not found!";
     } else {
-    // Parse returned JSON string
-    // Access index 0 for object
-    // Access value at 'description' key
-      console.log(JSON.parse(body)[0]['description']);
+      desc = JSON.parse(body)[0].description;
     }
-  }
-});
+
+    callback(error, desc);
+  });
+};
+
+module.exports = { fetchBreedDescription };
